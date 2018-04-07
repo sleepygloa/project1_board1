7b47acbc162c01b766e736136cf452b6eede574b
sleepygloa <sleepygloa@gmail.com>
2018-04-07 15:40:08 +0900

    change insertBlogForm in writeForm -> modal in MainPG

--- src/main/webapp/WEB-INF/views/main/main_content.jsp
+++ src/main/webapp/WEB-INF/views/main/main_content.jsp
@@ -1,4 +1,4 @@
-
+<%@ page language="java" contentType="text/html; charset=UTF-8" pageEncoding="UTF-8"%>
 		<div id="main" class="page-body">
 			<section class="page-index">
 				<div class="introBox" style="opacity: 1;">
@@ -12,7 +12,56 @@
 
 			<div class="page-content-title">Learning Paths</div>
 			
+			
+			<section class="cont-head">
+				<div class="container">
+					<p id="menu_count" class="cont-head-menu">
+						<strong id="blogContents_totalSectionCounts"></strong> sections
+					</p>
+					<p id="content_count" class="cont-head-title">
+						<strong id="blogContents_totalCounts"></strong> lessons
+					</p>
+					<div  class="pull-right m-t-10" data-toggle="modal" data-target="#blogAddModal"><i class="fa fa-lg fa-plus"></i></div>
+				</div>
+			</section>
 	    </div>
+	    
+	    <div class="modal modal-center fade" id="blogAddModal" tabindex="-1" role="dialog" aria-labelledby="myCenterModalLabel">
+			<div class="modal-dialog modal-center" role="document">
+				<div class="modal-content">
+				
+					<!-- Modal content-->
+					<div class="modal-content">
+						<div class="modal-header">
+							<button type="button" class="close" data-dismiss="modal">×</button>
+							<h4 class="modal-title">제목등록</h4>
+						</div>
+						<div class="modal-body">
+							<div class="col-xs-12 row m-b-10">
+					  			<div class="col-xs-6">
+						  			<button id="blogAddMenuSelect" type="button" class="btn btn-default input-lg dropdown-toggl col-xs-12" data-toggle="dropdown" aria-expanded="false">
+						  				메뉴선택 <span class="caret"></span>
+					  				</button>
+					  				<ul class="dropdown-menu col-xs-12" role="menu" id="blogAddDropdown"></ul>
+			  					</div>
+				  				<div class="col-xs-6">
+				  					<input type="text" class="input-lg col-xs-12" id="blogAddSetTitle" />
+			  					</div>
+							</div>
+			  				<div class="col-xs-12 m-b-10">
+			  					<label>SUBJECT</label>
+			  					<input type="text" class="input-lg col-xs-12" id="blogAddSetSubject" />
+			  				</div>
+						</div>
+						<div class="modal-footer">
+							<button type="button" class="btn btn-default btn-success" id="blogAddSubmit">SAVE</button>
+							<button type="button" class="btn btn-default" data-dismiss="modal">CLOSE</button>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div> 
+	    
 		<footer>
 		  <div class="container">
 		    <p class="social-icons">
@@ -21,7 +70,7 @@
 		      </a>
 		 	</p>
 		
-		    <p class="copyright">� 2018&nbsp;&nbsp;KIM SEONHO</p>
+		    <p class="copyright">© 2018&nbsp;&nbsp;KIM SEONHO</p>
 			  <p class="theme-by">Hosted on
 			    <a href="https://github.com/sleepygloa">GitHub Pages&nbsp;
 		        	<i class="fa fa-github"></i>
--- src/main/webapp/resources/js/main_content.js
+++ src/main/webapp/resources/js/main_content.js
@@ -7,6 +7,7 @@ var mainContentJs = function(){
 			
 			loadingMainContent();
 			
+			mainContentEvents();
 		}
 	}
 	
@@ -23,20 +24,9 @@ var mainContentJs = function(){
 					  title_subject_count = result.list[0].TOTAL_SUBJECT_COUNT;
 				  }
 				  
-				  var contentNavi = 
-					  '<section class="cont-head">'
-						+'<div class="container">'
-							+'<p id="menu_count" class="cont-head-menu">'
-							+'<strong id="blogContents_totalSectionCounts">'+title_count+'</strong> sections'
-							+'</p>'
-							+'<p id="content_count" class="cont-head-title">'
-							+'<strong id="blogContents_totalCounts">'+title_subject_count+'</strong> lessons'
-							+'</p>'
-							+'<div id="blogAddBtn" class="pull-right m-t-10"><i class="fa fa-lg fa-plus"></i></div>'
-						+'</div>'
-						+'</section>';
+				  $('#blogContents_totalSectionCounts').text(title_count);
+				  $('#blogContents_totalCounts').text(title_subject_count);
 				  
-				  $('#main').append(contentNavi);
 				  
 				  if(result.list){
 					  var list = result.list;
@@ -80,6 +70,80 @@ var mainContentJs = function(){
 				  
 				  $('#main').append(contentMain);
 			  }
+		  })
+	  }
+	  
+	  function mainContentEvents(){
+//		  $(document).on('click', '#blogAddBtn', function(){
+//			  $('#body').empty();
+//			  
+			  blogAddDropdown();
+//			  
+//			  var blogAddContent = '';
+//			  
+//			  blogAddContent = '<div class="container"><textarea id="blogAddSetContent" class="col-xs-12 p-0" placeholder="내용을 입력해주세요"></textarea></div>';
+//			  
+//			  $('#body').append(blogAddContent);
+//			  
+//		  })
+		  
+		  function blogAddDropdown(){
+			  $.ajax({
+				  url : "/main/getBlogAddDropdownList",
+				  success : function(result){
+					  var options = '';
+					  if(result.list){
+						  var list = result.list;
+						  for(var i in list){
+							  options += '<li class="blogAddDropdownLi">'+list[i].TITLE+'</li>';
+						  }  
+					  }
+					  options += '<li class="divider"></li>'
+						  +'<li class="blogAddDropdownLi">새 컨텐츠</a></li>';
+					  $('#blogAddDropdown').append(options);
+				  }
+			  })
+		  }
+		  
+		  $(document).on('click', '.blogAddDropdownLi', function(){
+			  var text = $(this).text();
+			  $('#blogAddSetTitle').val(text);
+		  })
+		  
+		  $(document).on('click', '#blogAddSubmit', function(){
+			  var menu = $('#blogAddSetTitle').val();
+			  var subject = $('#blogAddSetSubject').val();
+			  
+			  if(menu.length < 1){
+				  alert('메뉴를 선택해주세요');
+				  return false;
+			  }else if(subject.length < 1){
+				  alert('제목을 입력해주세요');
+				  return false;
+			  }
+			  
+			  var sendData = {
+					  title		: menu,
+					  subject	: subject
+			  }
+			  
+		      var jsonStr = JSON.stringify(sendData);
+			  
+			  $.ajax({
+				  url		: "/main/insertBlogAddContent",
+				  data  	: jsonStr,
+				  type		: "POST",
+				  contentType: 'application/json; charset=utf-8',
+				  success	: function(result){
+					  alert('글이 게시되었습니다.');
+					  window.location.href='/';
+				  }
+			  })
+			  
+		  })
+		  
+		  $(document).on('click', '#blogAddCancel', function(){
+			  window.location.href='/';
 		  })
 	  }
 	  
