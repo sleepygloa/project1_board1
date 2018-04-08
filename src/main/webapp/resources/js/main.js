1b07d33d1a5adf2c0b7a9d6f330372b00f93d78f
sleepygloa <sleepygloa@gmail.com>
2018-04-08 16:12:27 +0900

    Revert "change insertBlogForm in writeForm -> modal in MainPG"
    This reverts commit 7b47acbc162c01b766e736136cf452b6eede574b.

--- src/main/webapp/resources/js/main.js
+++ src/main/webapp/resources/js/main.js
@@ -222,6 +222,107 @@ var mainJs = function(){
 			  })
 		  })
 		  
+		  $(document).on('click', '#blogAddBtn', function(){
+			  $('#body').empty();
+			  
+			  var blogAddTitle = '';
+			  
+			  blogAddTitle += '<div class="container" style="background:gray; margin-top:100px;">'
+				  			+'<div class="btn-group col-xs-1">'
+					  			+'<button id="blogAddMenuSelect" type="button" class="btn btn-default input-lg dropdown-toggl col-xs-12" data-toggle="dropdown" aria-expanded="false">'
+					  				+'메뉴선택 <span class="caret"></span>'
+				  				+'</button>'
+					  				+'<ul class="dropdown-menu" role="menu" id="blogAddDropdown"></ul>'
+					  			+'</div>'
+					  			+'<input type="text" class="input-lg col-xs-2" id="blogAddSetTitle" />'
+					  			+'<input type="text" class="input-lg col-xs-4" id="blogAddSetSubject" />'
+					  			+'<span class="col-xs-1"><i class="fa fa-sm fa-plus" id="blogAddSubmit" ></i></span>'
+					  			+'<span class="col-xs-1"><i class="fa fa-sm fa-undo" id="blogAddCancel" ></i></span>'
+			  				+'</div>';
+			  
+			  $('#body').append(blogAddTitle);
+			  
+			  blogAddDropdown();
+			  
+			  var blogAddContent = '';
+			  
+			  blogAddContent = '<div class="container"><textarea id="blogAddSetContent" class="col-xs-12 p-0" placeholder="내용을 입력해주세요"></textarea></div>';
+			  
+			  $('#body').append(blogAddContent);
+			  
+		  })
+		  
+		  function blogAddDropdown(){
+			  $.ajax({
+				  url : "/main/getBlogAddDropdownList",
+				  success : function(result){
+					  var options = '';
+					  if(result.list){
+						  var list = result.list;
+						  for(var i in list){
+							  options += '<li class="blogAddDropdownLi">'+list[i].TITLE+'</li>';
+						  }  
+					  }
+					  options += '<li class="divider"></li>'
+						  +'<li class="blogAddDropdownLi">새 컨텐츠</a></li>';
+					  $('#blogAddDropdown').append(options);
+				  }
+			  })
+		  }
+		  
+		  $(document).on('click', '.blogAddDropdownLi', function(){
+			  var text = $(this).text();
+			  $('#blogAddSetTitle').val(text);
+		  })
+		  
+		  $(document).on('click', '#blogAddSubmit', function(){
+			  var menu = $('#blogAddSetTitle').val();
+			  var subject = $('#blogAddSetSubject').val();
+			  var content = $('#blogAddSetContent').val();
+			  
+			  console.log(menu);
+			  console.log(menu.length);
+			  console.log(subject.length);
+			  console.log(content);
+			  if(menu.length < 1){
+				  alert('메뉴를 선택해주세요');
+				  return false;
+			  }else if(subject.length < 1){
+				  alert('제목을 입력해주세요');
+				  return false;
+			  }else if(content.length < 1){
+				  alert('내용을 입력해주세요');
+				  return false;
+			  }
+			  
+			  var sendData = {
+					  title		: menu,
+					  subject	: subject,
+					  content	: content
+			  }
+			  
+		      var jsonStr = JSON.stringify(sendData);
+			  
+			  $.ajax({
+				  url		: "/main/insertBlogAddContent",
+				  data  	: jsonStr,
+				  type		: "POST",
+				  contentType: 'application/json; charset=utf-8',
+				  success	: function(result){
+					  alert('글이 게시되었습니다.');
+					  window.location.href='/';
+				  },
+				  error : function(result){
+					  alert('에러');
+				  }
+			  })
+			  
+		  })
+		  
+		  $(document).on('click', '#blogAddCancel', function(){
+			  window.location.href='/';
+		  })
+		  
 		  
 	  }
 	
