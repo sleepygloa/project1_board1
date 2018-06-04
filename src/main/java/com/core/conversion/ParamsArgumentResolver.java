94382cc3d5482e40ba324fbb227b96d4f73f8702
sleepygloa <sleepygloa@gmail.com>
2018-06-04 16:32:47 +0900

    수정) 파라메터 주석 추가

--- src/main/java/com/core/conversion/ParamsArgumentResolver.java
+++ src/main/java/com/core/conversion/ParamsArgumentResolver.java
@@ -27,6 +27,8 @@ import com.core.parameters.Params;
 import com.core.parameters.ParamsFactory;
 import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.paragon.core.conversion.CommDataTable;
+import com.paragon.core.conversion.DataTable;
 
 
 
@@ -37,34 +39,33 @@ public class ParamsArgumentResolver
   implements HandlerMethodArgumentResolver
 {
   private static final Log LOG = LogFactory.getLog(ParamsArgumentResolver.class);
-  
+
   public ParamsArgumentResolver() {}
-  
+
   public boolean supportsParameter(MethodParameter parameter) { return Params.class.isAssignableFrom(parameter.getParameterType()); }
-  
+
 
   public Object resolveArgument(MethodParameter parameter, ModelAndViewContainer mavContainer, NativeWebRequest webRequest, WebDataBinderFactory binderFactory)
     throws Exception
   {
+    //NativeWebReqeust 는 기본 Request를 오버라이딩함.
     HttpServletRequest req = (HttpServletRequest)webRequest.getNativeRequest();
     String contentType = StringUtils.defaultString(req.getContentType());
-    
+
     Map<String, String[]> paramMap = webRequest.getParameterMap();
-    
+
     Iterator<String> it = paramMap.keySet().iterator();
     Object key = null;
     String[] value = null;
     Params commParams = new CommParams();
-    boolean isMultipart = ServletFileUpload.isMultipartContent(req);
-    
-    HttpSession session = req.getSession();
+    boolean isMultipart = ServletFileUpload.isMultipartContent(req);//파일전송시 POST방식인지 체크
+
+    HttpSession session = req.getSession(); //세션 정보
     Enumeration<String> attrNames = session.getAttributeNames();
-    commParams.setParam("s_userId", session.getAttribute("s_userId"));
-    Set<String> addedParams = (Set)session.getAttribute("addedParams");
-    
+    commParams.setParam("s_userId", session.getAttribute("s_userId")); //로그인아이디 Param에 저장
+    Set<String> addedParams = (Set)session.getAttribute("addedParams"); //필수값들 Param에 저장
 
-
-
+    //세션에 추가된 정보와 필수값 Param와 일치하면, 세션정보를 Param에 저장
     if ((addedParams != null) && (addedParams.size() > 0)) {
       while (attrNames.hasMoreElements()) {
         String attrName = (String)attrNames.nextElement();
@@ -73,46 +74,67 @@ public class ParamsArgumentResolver
         }
       }
     }
+
+    //request의 parameter 정보를 Param에 저장
     while (it.hasNext())
     {
       key = it.next();
       value = (String[])paramMap.get(key);
       commParams.setParam(key.toString(), value[0]);
     }
-    
+
     if (contentType.contains("application/json")) {
-      webRequest.setAttribute("contentType", "application/json", 0);
-      StringBuilder jsonText = new StringBuilder();
-      String line = null;
-      ObjectMapper mapper = new ObjectMapper();
-      try {
-        BufferedReader reader = req.getReader();
-        while ((line = reader.readLine()) != null) {
-          jsonText.append(line);
-        }
-        
-        String jsonTxt = jsonText.toString();
-        if ("".equals(jsonTxt)) {
-        }else {
-            Map<String, Object> jsonMap = (Map<String, Object>)mapper.readValue(jsonTxt, new TypeReference<Map<String, Object>>() {});
-            for (Iterator<Map.Entry<String, Object>> i = jsonMap.entrySet().iterator(); i.hasNext();) {
-              Map.Entry<String, Object> e = (Map.Entry)i.next();
-              String jsonKey = (String)e.getKey();
-              Object jsonValue = e.getValue();
-              
-              commParams.setParam(jsonKey, jsonValue);
-            }
-        }
-      }
-      catch (Exception e)
-      {
-        e.printStackTrace();
-      }
-    } else if (isMultipart)
-    {
+          webRequest.setAttribute("contentType", "application/json", 0);
+          StringBuilder jsonText = new StringBuilder();
+          String line = null;
+          ObjectMapper mapper = new ObjectMapper();
+          try {
+                BufferedReader reader = req.getReader();
+                while ((line = reader.readLine()) != null) {
+                  jsonText.append(line);
+                }
+
+                String jsonTxt = jsonText.toString();
+                if ("".equals(jsonTxt)) {
+                }else {
+                    Map<String, Object> jsonMap = (Map<String, Object>)mapper.readValue(jsonTxt, new TypeReference<Map<String, Object>>() {});
+                    for (Iterator<Map.Entry<String, Object>> i = jsonMap.entrySet().iterator(); i.hasNext();) {
+                      Map.Entry<String, Object> e = (Map.Entry)i.next();
+                      String jsonKey = (String)e.getKey();
+                      Object jsonValue = e.getValue();
+
+
+                      //Request 중 dt_로 시작하는 데이터가 있으면 datatable로 받음
+                      if (jsonKey.startsWith("dt_")) {
+                          LOG.debug("dataTable make : " + jsonKey);
+
+                          List<Map<String, Object>> list = (List)jsonValue;
+                          DataTable dt = new CommDataTable();
+                          if ((addedParams != null) && (addedParams.size() > 0)) {
+                            LOG.debug("Add Session default Paramaters Size: " + addedParams.size());
+                            for (String addParamskey : addedParams) {
+                              if (addedParams.contains(addParamskey)) {
+                                dt.setParam(addParamskey, commParams.getParam(addParamskey));
+                              }
+                            }
+                          }
+                          dt.setDataTable(list, addedParams);
+                          commParams.setDataTable(jsonKey, dt);
+                        } else {
+                          commParams.setParam(jsonKey, jsonValue);
+                        }
+                    }
+                }
+
+          }
+          catch (Exception e)
+          {
+            e.printStackTrace();
+          }
+    } else if (isMultipart) {
       MultipartHttpServletRequest multipartHttpServletRequest = (MultipartHttpServletRequest)req;
       Iterator<String> iterator = multipartHttpServletRequest.getFileNames();
-      
+
       commParams.setFileable(true);
       while (iterator.hasNext()) {
         String fileKey = (String)iterator.next();
@@ -122,7 +144,7 @@ public class ParamsArgumentResolver
         }
       }
     }
-    
+
     return ParamsFactory.convertParmas(commParams);
   }
 }
--- src/main/java/com/core/conversion/ParamsReturnValueHandler.java
+++ src/main/java/com/core/conversion/ParamsReturnValueHandler.java
@@ -14,58 +14,48 @@ import com.core.parameters.Params;
 import com.core.parameters.datatable.DataTable;
 import com.core.web.servlet.JacksonView;
 
+public class ParamsReturnValueHandler implements HandlerMethodReturnValueHandler {
 
-public class ParamsReturnValueHandler
-  implements HandlerMethodReturnValueHandler
-{
-  private static final JacksonView JACKSON_VIEW = new JacksonView();
-  
-  public ParamsReturnValueHandler() {}
-  
-  public boolean supportsReturnType(MethodParameter returnType) { if (Params.class.isAssignableFrom(returnType.getParameterType()))
-      return true;
-    if (DataTable.class.isAssignableFrom(returnType.getParameterType())) {
-      return true;
-    }
-    return false;
-  }
-  
+    private static final JacksonView JACKSON_VIEW = new JacksonView();
 
-  public void handleReturnValue(Object returnVal, MethodParameter returnType, ModelAndViewContainer mavContainer, NativeWebRequest webRequest)
-    throws Exception
-  {
-    if (DataTable.class.isAssignableFrom(returnType.getParameterType())) {
-      mavContainer.setView(JACKSON_VIEW);
-      List<HashMap<String, Object>> list = (List)returnVal;
-      mavContainer.addAttribute(list);
-      return;
+    public ParamsReturnValueHandler() {}
+
+    public boolean supportsReturnType(MethodParameter returnType) {
+       /** Param 과 DataTable 을 구현하고 있을 때 */
+       if (Params.class.isAssignableFrom(returnType.getParameterType())) return true;
+       if (DataTable.class.isAssignableFrom(returnType.getParameterType())) return true;
+       return false;
     }
-    
+
+
+    public void handleReturnValue(Object returnVal, MethodParameter returnType, ModelAndViewContainer mavContainer, NativeWebRequest webRequest) throws Exception {
+        if (DataTable.class.isAssignableFrom(returnType.getParameterType())) {
+        mavContainer.setView(JACKSON_VIEW);
+        List<HashMap<String, Object>> list = (List)returnVal;
+        mavContainer.addAttribute(list);
+        return;
+    }
+
     String ajaxType = webRequest.getHeader("AjaxType");
     Map<String, Object> returnValue = (Map)returnVal;
     Iterator<String> it = returnValue.keySet().iterator();
     Object key = null;
     Object value = null;
     while (it.hasNext()) {
-      key = it.next();
-      value = returnValue.get(key.toString());
-      
-
-
-
-      mavContainer.addAttribute((String)key, value);
+        key = it.next();
+        value = returnValue.get(key.toString());
+        mavContainer.addAttribute((String)key, value);
     }
-    
+
     if (!"raw".equals(ajaxType)) {
-      mavContainer.setView(JACKSON_VIEW);
+        mavContainer.setView(JACKSON_VIEW);
     } else {
-      Params params = (Params)returnValue;
-      mavContainer.setRequestHandled(true);
-      
-      if (params.containsKey("stsCd")) {
-        webRequest.setAttribute("stsCd", params.getStsCd(), 0);
-      }
-      
+        Params params = (Params)returnValue;
+        mavContainer.setRequestHandled(true);
+
+        if (params.containsKey("stsCd")) {
+            webRequest.setAttribute("stsCd", params.getStsCd(), 0);
+        }
     }
   }
 }
